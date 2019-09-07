708d34ccdb28c0b30b9ed7d5e484965157b6403f
NatalieKu <b86103018@ntu.edu.tw>
2019-09-07 15:53:10 +0800

    Initial Commit

---
+++ Bomb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bomb
+//
+//  Created by MEI KU on 2019/9/7.
+//  Copyright © 2019 Natalie KU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
